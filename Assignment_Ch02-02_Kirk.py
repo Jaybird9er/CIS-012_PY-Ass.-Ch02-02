c92cae851f1801c918e87d180bcb942c7d5d7d4d
Jamey Kirk <JameyKirk@gmail.com>
2022-01-14 23:08:37 +0900

    Initial commit

---
+++ Assignment_Ch02-02_Kirk.py
@@ -0,0 +1,6 @@
+firstName = input("Enter your first name: ")
+studyHours = float(input("Enter how many hours you have studied so far: "))
+
+print("\n")
+print("Hello, ", firstName, "!")
+print("On Saturday, you will need to study for an additional ", 10 - studyHours, " for the exam.")
